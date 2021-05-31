3bff5a72b21f695f9d9cc6f165cd253987b82c06
kvu-luong <luongkhanhvu1392@gmail.com>
2021-05-31 22:21:32 +0700

    feat(readme): add readme

---
+++ README.rb
@@ -0,0 +1,9 @@
+### DONE
+- play/pause: OK
+- change audio: OK
+- volume: slide -> OK; mute -> OK, button -> OK
+- audio slide duration: OK with input type range
+### REMAIN
+- lyric
+- cover to modules
+- recss
